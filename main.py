1732fcf8a866a15c0f3b97fb22c7f227ac0f8666
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-25 09:06:52 +0000

    feat: register lifespan handler and store limiter and app state

--- main.py
+++ main.py
@@ -1,47 +1,26 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+
+from fastapi import FastAPI
+
 from src.rate_limiter.algorithms.leaky_bucket import LeakyBucketAlgorithm
-from src.rate_limiter.core.request import RateLimitRequest
-import time
-
 from src.rate_limiter.middleware.middleware import HTTPRateLimiter
-
-app = FastAPI()
-
-limiter = LeakyBucketAlgorithm(bucket_size=5, leak_rate=1)
-app.add_middleware(HTTPRateLimiter, algorithm=limiter)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await limiter.start()
+    await app.state.limiter.start()
     yield
-    await limiter.stop()
+    await app.state.limiter.stop()
 
+app = FastAPI(lifespan=lifespan)
+
+app.state.limiter = LeakyBucketAlgorithm(bucket_size=5, leak_rate=1)
+app.add_middleware(HTTPRateLimiter, algorithm=app.state.limiter)
 
 @app.get("/")
-async def root(request: Request):
-    rate_limit_request = RateLimitRequest(
-        id=str(request.client.host),
-        timestamp=time.time(),
-        client_ip=request.client.host,
-        path=request.url.path,
-        method=request.method,
-    )
-
-    response = await limiter.allow_request(rate_limit_request)
-
-    if not response.is_allowed:
-        return JSONResponse(
-            status_code=429,
-            content={"error": "Too Many Requests"},
-            headers=response.headers,
-        )
-
+async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/status")
 async def status():
-    return await limiter.get_status()
+    return await app.state.limiter.get_status()
